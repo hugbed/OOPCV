417ddee2dde6e3ac5092be4a1be4109c8a3fdeb7
Hugo Bedard <hugo_bedard@hotmail.com>
2016-08-16 14:56:45 -0400

    Working Mesh

---
+++ include/Mesh.h
@@ -0,0 +1,37 @@
+//
+// Created by bedh2102 on 16/08/16.
+//
+
+#ifndef OPCVIEWER_MESH_H
+#define OPCVIEWER_MESH_H
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+
+#include <vector>
+
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 color;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices);
+    void Draw(Shader shader);
+
+private:
+    GLuint VAO, VBO, EBO;
+
+    void setupMesh();
+};
+
+
+
+#endif //OPCVIEWER_MESH_H
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <Mesh.h>
 
 #include "Camera.h"
 
@@ -101,44 +102,23 @@ void initViewport(GLFWwindow *window)
     glEnable(GL_DEPTH_TEST);
 }
 
-bool createVAO(GLuint &VAO)
+Mesh createTriangleMesh()
 {
-    GLfloat vertices[] = {
-            // Positions         // Colors
-            0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
-           -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
-            0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
+    Vertex vertices[] = {
+            // Positions                          // Colors
+            { glm::vec3( 0.5f, -0.5f, 0.0f),  glm::vec3(1.0f, 0.0f, 0.0f) },  // Bottom Right
+            { glm::vec3(-0.5f, -0.5f, 0.0f),  glm::vec3(0.0f, 1.0f, 0.0f) },  // Bottom Left
+            { glm::vec3( 0.0f,  0.5f, 0.0f),  glm::vec3(0.0f, 0.0f, 1.0f) }   // Top
     };
 
     GLuint indices[] = {  // Note that we start from 0!
-        0, 1, 2,  // First Triangle
+        0, 1, 2  // First Triangle
     };
-    GLuint VBO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    Mesh triangleMesh(std::vector<Vertex>(vertices, vertices + sizeof(vertices) / sizeof(vertices[0])),
+                      std::vector<GLuint>(indices, indices + sizeof(indices) / sizeof(indices[0])));
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
-
-    return true;
+    return triangleMesh;
 }
 
 int main() {
@@ -158,8 +138,7 @@ int main() {
 
     Shader shader("shaders/basic.vs", "shaders/basic.fs");
 
-    GLuint VAO;
-    createVAO(VAO);
+    Mesh triangleMesh = createTriangleMesh();
 
     glClearColor(0.f, 0.f, 0.f, 1.0f);
     glEnable(GL_PROGRAM_POINT_SIZE);
@@ -171,7 +150,7 @@ int main() {
                        glm::vec3(0.0f, 0.0f, 0.0f),
                        glm::vec3(0.0f, 1.0f, 0.0f));
 
-    glm::vec3 cubePositions[] = {
+    glm::vec3 trianglePositions[] = {
             glm::vec3( 0.0f,  0.0f,  0.0f),
             glm::vec3( 2.0f,  5.0f, -15.0f),
             glm::vec3(-1.5f, -2.2f, -2.5f),
@@ -210,23 +189,15 @@ int main() {
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-        // Render
-        // do great stuff here
-        glBindVertexArray(VAO);
-
         for(GLuint i = 0; i < 10; i++)
         {
             glm::mat4 model = glm::mat4();
-            model = glm::translate(model, cubePositions[i]);
+            model = glm::translate(model, trianglePositions[i]);
             GLfloat angle = 20.0f * i;
             model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+            triangleMesh.Draw(shader);
         }
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
